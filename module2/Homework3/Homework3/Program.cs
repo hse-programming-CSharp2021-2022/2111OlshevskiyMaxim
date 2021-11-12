acc95f91d0a0ee099c465967ca50a95e8176a78d
IPineappleI <mmolshevskiy@edu.hse.ru>
2021-11-12 05:58:04 +0300

    Update1

---
+++ module2/Homework3/Homework3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using static System.Console;
+
+namespace Homework3
+{
+    public class Program
+    {
+        private static readonly Random s_random = new();
+
+        private static void Main()
+        {
+            VideoFile videoFile = new("Отдельный видеофайл", 210, 550);
+            WriteLine($"{videoFile}\n\nМассив видеофайлов:");
+            VideoFile[] videoFiles = new VideoFile[s_random.Next(5, 16)];
+            for (int i = 0; i < videoFiles.Length; i++)
+            {
+                videoFiles[i] = new(GetRandomName(), s_random.Next(60, 361), s_random.Next(100, 1001));
+                WriteLine($"{i + 1}. {videoFiles[i]}");
+            }
+            WriteLine("\nВидеофайлы, размер которых больше, чем размер отдельного видеофайла:");
+            for (int i = 0; i < videoFiles.Length; i++)
+            {
+                if (videoFiles[i].Size > videoFile.Size)
+                    WriteLine($"{i + 1}. {videoFiles[i]}");
+            }
+        }
+
+        public static string GetRandomName()
+        {
+            char[] name = new char[s_random.Next(2, 10)];
+            for (int i = 0; i < name.Length; i++)
+            {
+                int randomCase = s_random.Next(1, 3);
+                if (randomCase == 1)
+                    name[i] = (char)s_random.Next('A', 'Z' + 1);
+                else
+                    name[i] = (char)s_random.Next('a', 'z' + 1);
+            }
+            return new(name);
+        }
+    }
+
+    public class VideoFile
+    {
+        public string Name { get; private set; }
+        public int Duration { get; private set; }
+        public int Quality { get; private set; }
+        public int Size => Duration * Quality;
+        public VideoFile(string name, int duration, int quality) =>
+            (Name, Duration, Quality) = (name, duration, quality);
+        public override string ToString() =>
+            $"Наименование видеофайла: {Name}; Длительность: {Duration} с; Качество: {Quality}; Размер: {Size}.";
+    }
+}
---
+++ module2/Seminar3/Seminar3/Program.cs
@@ -0,0 +1,40 @@
+﻿using static System.Console;
+
+namespace Seminar3
+{
+    abstract class Animal
+    {
+        public string Name { get; protected set; }
+        public string Age { get; protected set; }
+        public abstract string AnimalSound();
+        public abstract string AnimalInfo();
+    }
+
+    sealed class Dog : Animal
+    {
+        public string Breed { get; private set; }
+        public bool IsTrained { get; private set; }
+        public Dog(string name, string age, string breed, bool isTrained) => (Name, Age, Breed, IsTrained) = (name, age, breed, isTrained);
+        public override string AnimalSound() => "Гав.";
+        public override string AnimalInfo() => $"Кличка: {Name}; Возраст: {Age}; Порода: {Breed}; Дрессирована: {IsTrained}.";
+    }
+
+    sealed class Cow : Animal
+    {
+        public int MilkPerDay { get; private set; }
+        public Cow(string name, string age, int milkPerDay) => (Name, Age, MilkPerDay) = (name, age, milkPerDay);
+        public override string AnimalSound() => "Му.";
+        public override string AnimalInfo() => $"Кличка: {Name}; Возраст: {Age}; Количество молока в день: {MilkPerDay} литра.";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new("Бобик", "2 года", "Немецкая овчарка", true);
+            Cow cow = new("Люся", "3 года", 2);
+            WriteLine($"{dog.AnimalInfo()}\n{dog.AnimalSound()}");
+            WriteLine($"{cow.AnimalInfo()}\n{cow.AnimalSound()}");
+        }
+    }
+}
