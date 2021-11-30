fe1096fd43fc4e591fe87eef425cf2a7718da88f
IPineappleI <mmolshevskiy@edu.hse.ru>
2021-11-30 11:28:14 +0300

    Update6

---
+++ module2/Homework11/Homework11/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Homework11
+{
+    partial class ListBoxForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.restoreButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 15;
+            this.listBox.Items.AddRange(new object[] {
+            "один",
+            "два",
+            "три",
+            "четыре",
+            "пять",
+            "шесть",
+            "семь"});
+            this.listBox.Location = new System.Drawing.Point(339, 158);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(120, 124);
+            this.listBox.TabIndex = 0;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            // 
+            // restoreButton
+            // 
+            this.restoreButton.Location = new System.Drawing.Point(296, 119);
+            this.restoreButton.Name = "restoreButton";
+            this.restoreButton.Size = new System.Drawing.Size(206, 23);
+            this.restoreButton.TabIndex = 1;
+            this.restoreButton.Text = "Отобразить начальный список";
+            this.restoreButton.UseVisualStyleBackColor = true;
+            this.restoreButton.Click += new System.EventHandler(this.restoreButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Enabled = false;
+            this.deleteButton.Location = new System.Drawing.Point(296, 297);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(206, 23);
+            this.deleteButton.TabIndex = 2;
+            this.deleteButton.Text = "Удалить выбранный элемент";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // ListBoxForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.restoreButton);
+            this.Controls.Add(this.listBox);
+            this.Name = "ListBoxForm";
+            this.Text = "ListBox";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button restoreButton;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
+
