5e5e3e83fca1ce9dd561d868d45b88ffb9bee59b
IPineappleI <mmolshevskiy@edu.hse.ru>
2022-01-21 13:46:54 +0300

    Update5

---
+++ module3/Seminar02/Seminar02/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using static System.Console;
+
+namespace Seminar02
+{
+    delegate void SquareSizeChanged(double a, double b);
+
+    class Square
+    {
+        private Tuple<double, double> pointA;
+        private Tuple<double, double> pointB;
+
+        public Tuple<double, double> PointA
+        {
+            get => pointA;
+            set
+            {
+                pointA = value;
+                OnSizeChanged?.Invoke(pointB.Item1 - pointA.Item1, pointB.Item2 - pointA.Item2);
+            }
+        }
+
+        public Tuple<double, double> PointB
+        {
+            get => pointB;
+            set
+            {
+                pointB = value;
+                OnSizeChanged?.Invoke(pointB.Item1 - pointA.Item1, pointB.Item2 - pointA.Item2);
+            }
+        }
+
+        public Square(Tuple<double, double> a, Tuple<double, double> b) => (pointA, pointB) = (a, b);
+
+        public event SquareSizeChanged OnSizeChanged;
+    }
+
+    class Program
+    {
+        public static void SquareConsoleInfo(double a, double b) => WriteLine("Длины сторон: {0:f2} и {1:f2}", a, b);
+
+        static void Main(string[] args)
+        {
+            Write("Координаты верхнего левого угла через пробел: ");
+            string[] split = ReadLine().Split(' ');
+            Tuple<double, double> a = new(double.Parse(split[0]), double.Parse(split[1]));
+            Write("Координаты правого нижнего угла через пробел: ");
+            split = ReadLine().Split(' ');
+            Tuple<double, double> b = new(double.Parse(split[0]), double.Parse(split[1]));
+            Square s = new(a, b);
+            s.OnSizeChanged += SquareConsoleInfo;
+            while (true)
+            {
+                Write("Новые координаты правого нижнего угла через пробел: ");
+                split = ReadLine().Split(' ');
+                s.PointB = new(double.Parse(split[0]), double.Parse(split[1]));
+                WriteLine("Нажмите клавишу ESC, если хотите выйти.");
+                if (ReadKey().Key == ConsoleKey.Escape)
+                    return;
+            }
+        }
+    }
+}
---
+++ module3/Seminar03/Seminar03/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Seminar02
+{
+    public class UIString
+    {
+        public void NewStringValueHappenedHandler(object sender, MyArgs s) { str = s.S; }
+        string str = "Default text";
+        public string Str { get { return str; } private set { str = value; } }
+    }
+    class ConsoleUI
+    {
+        public event EventHandler<MyArgs> NewStringValueHappened;
+        UIString s = new UIString(); // специальная строка
+        public UIString S { get { return s; } set { s = value; } }
+        public void GetStringFromUI()
+        {
+            Console.Write("Введите новое значение строки: ");
+            string str = Console.ReadLine();
+            MyArgs myArgs = new() { S = str };
+            NewStringValueHappened?.Invoke(this, myArgs);
+            RefreshUI();
+        }
+        public void CreateUI()
+        {
+            RefreshUI();
+            NewStringValueHappened += s.NewStringValueHappenedHandler;
+        }
+        public void RefreshUI()
+        {      // обновление строки     
+            Console.Clear();
+            Console.WriteLine("Текст строки: " + s.Str);
+        }
+    }
+
+    public class MyArgs : EventArgs
+    {
+        public string S { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleUI c = new ConsoleUI();
+            c.CreateUI(); // запускаем выполнение объекта класса ConsoleUI
+            do
+            {
+                c.GetStringFromUI();  // изменяем значение строки
+                Console.WriteLine("Чтобы закончить эксперименты, нажмите ESC...");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
